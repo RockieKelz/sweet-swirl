16f23b5abdf79840938cecf0a621c48a363137f8
RockieKelz <racquy@msn.com>
2026-09-20 00:58:34 -0700

    Added icons with dragging action to the game board

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+export { default as blue } from './blue.png';
+export { default as green } from './green.png';
+export { default as orange } from './orange.png';
+export { default as pink } from './pink.png';
+export { default as purple } from './purple.png';
+export { default as red } from './red.png';
+export { default as white } from './white.png';
+
--- src/game/GameBoard.jsx
+++ src/game/GameBoard.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react'
+import { blue, green, orange, pink, purple, red, white } from '../assets'
 import './Gameboard.css'
 
+const cellColors = [
+            'url(' + blue + ')',
+            'url(' + green + ')',
+            'url(' + orange + ')',
+            'url(' + pink + ')',
+            'url(' + purple + ')',
+            'url(' + red + ')',
+            'url(' + white + ')'
+        ]
+        
 function Gameboard() {
+    //create the gameboard  
+    const [squares, setSquares] = useState([])
+    const width = 6
+    
+    useEffect(() => {
+        const randomSquares = []
+        for (let i = 0; i < width*width; i++) {
+            const randomColor = cellColors[Math.floor(Math.random() * cellColors.length)]
+            randomSquares.push(randomColor)
+        }
+        setSquares(randomSquares)
+    }
+    , [])
 
+        
     return (
         <div className="game-container">
             <header className="game-header">
@@ -12,9 +38,27 @@ function Gameboard() {
                     <h3>score</h3>
                     <h1 id="score"></h1> 
                 </section>
-                <div className={`grid`} />
+                <div className={`grid`}>
+                    {squares.map((color, index) => (
+                        <div 
+                            key={index}                      
+                            id={index}
+                            draggable="true"
+o                           onDragStart={(e) => e.dataTransfer.setData("text/plain", index)}
+                            onDragOver={(e) => e.preventDefault()}
+                            onDrop={(e) => {
+                                const draggedIndex = e.dataTransfer.getData("text/plain")
+                                const targetIndex = index
+                                const newSquares = [...squares]
+                                newSquares[targetIndex] = squares[draggedIndex]
+                                newSquares[draggedIndex] = squares[targetIndex]
+                                setSquares(newSquares)}}
+                        style={{ backgroundImage: color }}></div>
+                    ))}
+                </div>
             </main>
         </div>
     )
 }
+
 export default Gameboard
